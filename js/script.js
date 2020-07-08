ea1f4bad69681f8c425ddd7b37020c6926614e1f
Yirzaraji <not.iremy@gmail.com>
2020-07-08 12:24:28 +0200

    listing statique ville bouton hide show et search bar en Vue Js

--- js/script.js
+++ js/script.js
@@ -3,6 +3,38 @@ console.log("test")
 let app = new Vue({
     el: '#app',
     data: {
-        message: 'App Meteo'
+        search: '',
+        message: 'App Meteo',
+
+        cities: [
+            { name: 'Paris' },
+            { name: 'Athene' },
+            { name: 'Tokyo' },
+            { name: 'Osaka' },
+            { name: 'Moscow' },
+            { name: 'Los Santos' }
+        ],
+
+        showCities: false,
+    },
+    computed: {
+        filteredCities: function() {
+            var self = this;
+            return this.cities.filter(function(cust) { return cust.name.toLowerCase().indexOf(self.search.toLowerCase()) >= 0; });
+            //return this.customers;
+        }
     }
+
 })
+
+// sert a rien pour l'instant`
+Vue.component('button-counter', {
+    data: function() {
+        return {
+            count: 0
+        }
+    },
+    template: '<button v-on:click="count++">Vous m\'avez cliqué {{ count }} fois.</button>'
+})
+
+//new Vue({ el: '#components-demo' })
